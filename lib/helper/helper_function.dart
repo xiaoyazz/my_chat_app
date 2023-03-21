64bae33d57da17fb1af0ec31ae0c0abb20c7ee1a
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2023-03-20 21:35:41 -0400

    Update helper_functions

---
+++ lib/helper/helper_function.dart
@@ -0,0 +1,10 @@
+class HelperFunctions {
+  // Keys
+  static String userLoggedInKey = "LOGGEDINKEY";
+  static String userNameKey = "USERNAMEKEY";
+  static String userEmailKet = "USEREMAILKEY";
+
+  // Save the data to SF
+
+  // Get the data from SF
+}
--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,21 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
