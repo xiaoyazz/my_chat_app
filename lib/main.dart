9fc69c8b728747a73d0c66df5d6b1273772cbe17
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2023-03-20 21:23:20 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:my_chat_app/pages/home_page.dart';
+import 'package:my_chat_app/shared/constants.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Check if the current version is web or not
+  if (kIsWeb) {
+    // Run the initialization for web
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    // Run the initialization for android and iOS
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+// All string values are from firebase project overview > project settings > web
+class Constants {
+  static String appId = "1:154361087501:web:e755739c86547c37ba3744";
+  static String apiKey = "AIzaSyBTSXDnah80rVklLtIlKhnKUQvyNziE3X0";
+  static String messagingSenderId = "154361087501";
+  static String projectId = "mychatapp-aceb8";
+}
