b6d03e76f8737b53d7819bd698d114e7fb9a539f
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2023-03-20 23:03:08 -0400

    Add login page and Home page

---
+++ lib/pages/auth/login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:matcher/matcher.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    // Test if LoginPage works
+    // return Scaffold(
+    //   body: Center(child: Text("LoginPage")),
+    // );
+
+    //
+    return Scaffold();
+  }
+}
