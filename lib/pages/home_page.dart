7f2bd1d1195e55bdb0fd24e40ec797cf9811796b
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2023-04-04 00:06:37 -0400

    Add Group Creation feature

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,12 @@
+import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
+import 'package:flutter/widgets.dart';
 import "package:my_chat_app/helper/helper_function.dart";
+import "package:my_chat_app/pages/auth/login_page.dart";
+import "package:my_chat_app/pages/profile_page.dart";
 import "package:my_chat_app/pages/search_page.dart";
 import "package:my_chat_app/service/auth_service.dart";
+import "package:my_chat_app/service/database_service.dart";
 import "package:my_chat_app/widgets/widgets.dart";
 
 class HomePage extends StatefulWidget {
@@ -15,6 +20,9 @@ class _HomePageState extends State<HomePage> {
   String userName = "";
   String email = "";
   AuthService authService = AuthService();
+  Stream? groups;
+  bool _isLoading = false;
+  String groupName = "";
 
   // when initialize
   @override
@@ -30,98 +38,351 @@ class _HomePageState extends State<HomePage> {
       });
     });
     await HelperFunctions.getUserNameFromSF().then((val) {
-      userName = val!;
+      setState(() {
+        userName = val!;
+      });
+    });
+
+    // Get the list of snapshots in our stream
+    await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
+        .getUserGroup()
+        .then((snapshot) {
+      groups = snapshot;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: Center(
-      //     child: ElevatedButton(
-      //   child: Text("LOGOUT"),
-      //   onPressed: () {
-      //     authService.signOut();
-      //   },
-      // )),
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                nextScreen(context, const SearchPage());
-              },
-              icon: const Icon(Icons.search))
-        ],
-        backgroundColor: Theme.of(context).primaryColor,
-        centerTitle: true,
-        title: const Text(
-          "Party Group",
-          style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
+        // body: Center(
+        //     child: ElevatedButton(
+        //   child: Text("LOGOUT"),
+        //   onPressed: () {
+        //     authService.signOut();
+        //   },
+        // )),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  nextScreen(context, const SearchPage());
+                },
+                icon: const Icon(Icons.search))
+          ],
+          backgroundColor: Theme.of(context).primaryColor,
+          centerTitle: true,
+          title: const Text(
+            "Party Groups",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
+          ),
         ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 50),
-          children: <Widget>[
-            Icon(
-              Icons.account_circle,
-              size: 150,
-              color: Colors.grey[700],
+        drawer: Drawer(
+          child: ListView(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            children: <Widget>[
+              Icon(
+                Icons.account_circle,
+                size: 150,
+                color: Colors.grey[700],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                userName,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Divider(
+                height: 2,
+              ),
+              ListTile(
+                onTap: () {},
+                selectedColor: Theme.of(context).primaryColor,
+                selected: true,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                leading: const Icon(Icons.group),
+                title: const Text(
+                  "Groups",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              ListTile(
+                onTap: () async {},
+                selectedColor: Theme.of(context).primaryColor,
+                selected: false,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                leading: const Icon(Icons.event),
+                title: const Text(
+                  "Canlendar",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              ListTile(
+                onTap: () async {},
+                selectedColor: Theme.of(context).primaryColor,
+                selected: false,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                leading: const Icon(Icons.celebration),
+                title: const Text(
+                  "Events",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              ListTile(
+                onTap: () async {
+                  nextScreen(
+                      context,
+                      ProfilePage(
+                        userName: userName,
+                        email: email,
+                      ));
+                },
+                selectedColor: Theme.of(context).primaryColor,
+                selected: false,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                leading: const Icon(Icons.account_circle),
+                title: const Text(
+                  "Profile",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              ListTile(
+                onTap: () async {},
+                selectedColor: Theme.of(context).primaryColor,
+                selected: false,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                leading: const Icon(Icons.auto_awesome),
+                title: const Text(
+                  "Party Tips",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              ListTile(
+                onTap: () async {},
+                selectedColor: Theme.of(context).primaryColor,
+                selected: false,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                leading: const Icon(Icons.help),
+                title: const Text(
+                  "Help & Support",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              ListTile(
+                onTap: () async {
+                  showDialog(
+                      barrierDismissible: false,
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text("Logout"),
+                          content:
+                              const Text("Are you sure you want to log out?"),
+                          actions: [
+                            IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(
+                                Icons.cancel,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () async {
+                                await authService.signOut();
+                                nextScreenReplace(context, const LoginPage());
+                                // Navigator.of(context).pushAndRemoveUntil(
+                                //     MaterialPageRoute(
+                                //         builder: (context) => const LoginPage()),
+                                //     (route) => false);
+                              },
+                              icon: const Icon(
+                                Icons.check_circle,
+                                color: Colors.green,
+                              ),
+                            )
+                          ],
+                        );
+                      });
+
+                  // authService.signOut().whenComplete(() {
+                  //   nextScreenReplace(context, const LoginPage());
+                  // });
+                },
+                selectedColor: Theme.of(context).primaryColor,
+                selected: false,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                leading: const Icon(Icons.exit_to_app),
+                title: const Text(
+                  "Log Out",
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          ),
+        ),
+        body: groupList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            popUpDialog(context);
+          },
+          elevation: 0,
+          backgroundColor: Theme.of(context).primaryColor,
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 30,
+          ),
+        ));
+  }
+
+  popUpDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: ((context, setState) {
+            return AlertDialog(
+              title: const Text(
+                "Enter your party code",
+                textAlign: TextAlign.left,
+              ),
+              // Dismiss the create popup
+              content: Column(mainAxisSize: MainAxisSize.min, children: [
+                _isLoading == true
+                    ? Center(
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      )
+                    : TextField(
+                        onChanged: (val) {
+                          setState(() {
+                            groupName =
+                                val; // In the final app, need to validate if the groupName(party code) is valid
+                          });
+                        },
+                        style: const TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                              borderRadius: BorderRadius.circular(10)),
+                          errorBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Colors.red,
+                              ),
+                              borderRadius: BorderRadius.circular(10)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+              ]),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pop(); // Make the cancel button functional
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor),
+                  child: const Text("CANCEL"),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (groupName != "") {
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      DatabaseService(
+                              uid: FirebaseAuth.instance.currentUser!.uid)
+                          .createGroup(userName,
+                              FirebaseAuth.instance.currentUser!.uid, groupName)
+                          .whenComplete(() {
+                        _isLoading = false;
+                      });
+                      Navigator.of(context).pop();
+                      showSnackBar(context, Colors.green,
+                          "Welcome to your party group!");
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor),
+                  child: const Text("ENABLE"),
+                ),
+              ],
+            );
+          }));
+        });
+  }
+
+  groupList() {
+    return StreamBuilder(
+      stream: groups,
+      builder: (context, AsyncSnapshot snapshot) {
+        // Make some checks before return
+        if (snapshot.hasData) {
+          if (snapshot.data['groups'] != null) {
+            if (snapshot.data['groups'].length != 0) {
+              return Text("Helloooo");
+            } else {
+              return noGroupWidget();
+            }
+          } else {
+            return noGroupWidget();
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor),
+          );
+        }
+      },
+    );
+  }
+
+  noGroupWidget() {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                popUpDialog(context);
+              },
+              child: Icon(
+                Icons.add_circle,
+                color: Colors.grey[700],
+                size: 75,
+              ),
             ),
             const SizedBox(
-              height: 15,
+              height: 20,
             ),
-            Text(
-              userName,
+            const Text(
+              "You haven't joined any groups. If you are a party host, please tab on the add button to create your party group. If you are a guest, please search and join a group from the top search button.",
               textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Divider(
-              height: 2,
-            ),
-            ListTile(
-              onTap: () {},
-              selectedColor: Theme.of(context).primaryColor,
-              selected: true,
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              leading: const Icon(Icons.group),
-              title: const Text(
-                "My party groups",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              selectedColor: Theme.of(context).primaryColor,
-              selected: false,
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              leading: const Icon(Icons.group),
-              title: const Text(
-                "My party groups",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              selectedColor: Theme.of(context).primaryColor,
-              selected: false,
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              leading: const Icon(Icons.group),
-              title: const Text(
-                "My party groups",
-                style: TextStyle(color: Colors.black),
-              ),
             )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -39,4 +39,34 @@ class DatabaseService {
         await userCollection.where("email", isEqualTo: email).get();
     return snapshot;
   }
+
+  // Get user groups
+  getUserGroup() async {
+    return userCollection.doc(uid).snapshots();
+  }
+
+  // Create a group
+  Future createGroup(String userName, String id, String groupName) async {
+    DocumentReference groupDocumentReference = await groupCollection.add({
+      "groupName": groupName,
+      "groupIcon": "",
+      "admin": "${id}_$userName",
+      "members": [],
+      "groupId": "", // we don't have it yet
+      "recentMessageSender": "",
+    });
+    // After the group created, we create a document reference
+    // and update the member
+    await groupDocumentReference.update({
+      "members": FieldValue.arrayUnion(["${uid}_$userName"]),
+      "groupId":
+          groupDocumentReference.id, // generated after the group is created
+    });
+
+    DocumentReference userDocumentReference = await userCollection.doc(uid);
+    return await userDocumentReference.update({
+      "groups":
+          FieldValue.arrayUnion(["${groupDocumentReference.id}_$groupName"])
+    });
+  }
 }
