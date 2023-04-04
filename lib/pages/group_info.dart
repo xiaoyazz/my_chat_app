9003842310e9087755315751b6ed2b6982e6e38a
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2023-04-04 03:16:12 -0400

    Add group info page

---
+++ lib/pages/group_info.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:my_chat_app/service/database_service.dart';
+
+class GroupInfo extends StatefulWidget {
+  final String groupId;
+  final String groupName;
+  final String adminName;
+
+  const GroupInfo(
+      {Key? key,
+      required this.groupId,
+      required this.groupName,
+      required this.adminName})
+      : super(key: key);
+
+  @override
+  State<GroupInfo> createState() => _GroupInfoState();
+}
+
+class _GroupInfoState extends State<GroupInfo> {
+  Stream? members;
+  @override
+  void initState() {
+    getMembers();
+    super.initState();
+  }
+
+  getMembers() async {
+    DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
+        .getGroupMembers(widget.groupId)
+        .then((val) {
+      setState(() {
+        members = val;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text("Group Info"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.exit_to_app))
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Theme.of(context).primaryColor.withOpacity(0.2)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Theme.of(context).primaryColor,
+                  child: Text(
+                    widget.groupName.substring(0, 1).toUpperCase(),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w500, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
